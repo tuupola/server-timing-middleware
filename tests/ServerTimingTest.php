ad5fd0e2b5a4d6aab61bed12d2a0e29413d92a58
Mika Tuupola <tuupola@appelsiini.net>
2017-04-02 10:49:57 +0700

    Test RFC7230 token generation

--- tests/ServerTimingTest.php
+++ tests/ServerTimingTest.php
@@ -15,11 +15,11 @@
 
 namespace Tuupola\Middleware;
 
+use Equip\Dispatch\MiddlewarePipe;
+use Tuupola\Middleware\ServerTiming\Stopwatch;
 use Zend\Diactoros\ServerRequest as Request;
 use Zend\Diactoros\Response;
 use Zend\Diactoros\Uri;
-
-use Equip\Dispatch\MiddlewarePipe;
 
 class ServerTimingTest extends \PHPUnit_Framework_TestCase
 {
@@ -77,6 +77,34 @@ class ServerTimingTest extends \PHPUnit_Framework_TestCase
 
         $header = $response->getHeader("Server-Timing")[0];
         $regex = "/Bootstrap=[0-9\.]+, Process=[0-9\.]+, Total=[0-9\.]+/";
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Success", $response->getBody());
+        $this->assertTrue((boolean) preg_match($regex, $header));
+    }
+
+    /* https://tools.ietf.org/html/rfc7230#section-3.2.6 */
+    public function testShouldGenerateValidToken()
+    {
+        $request = (new Request())
+            ->withUri(new Uri("https://example.com/"))
+            ->withMethod("GET");
+
+        $response = new Response;
+
+        $next = function (Request $request, Response $response) {
+            $response->getBody()->write("Success");
+            return $response;
+        };
+
+        $stopwatch = new Stopwatch;
+        $stopwatch->set("DB Server", 100);
+
+        $timing = new ServerTiming($stopwatch);
+        $response = $timing($request, $response, $next);
+
+        $header = $response->getHeader("Server-Timing")[0];
+        $regex = '/^dbserver=0.100; "DB Server", Bootstrap=[0-9\.]+, Process=[0-9\.]+, Total=[0-9\.]+/';
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals("Success", $response->getBody());
