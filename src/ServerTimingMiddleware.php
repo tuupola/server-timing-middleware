723ad6321e47ba1628dc70f00ab172c83d368acd
Mika Tuupola <tuupola@appelsiini.net>
2018-01-23 14:25:21 +0700

    Use approved PSR-15

--- src/ServerTimingMiddleware.php
+++ src/ServerTimingMiddleware.php
@@ -16,8 +16,8 @@
 
 namespace Tuupola\Middleware;
 
-use Interop\Http\Server\MiddlewareInterface;
-use Interop\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Tuupola\Middleware\ServerTiming\CallableHandler;
