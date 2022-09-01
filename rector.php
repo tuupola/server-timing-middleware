008d4ff578e14bc4c388628506bf481cc2143b5c
Mika Tuupola <tuupola@appelsiini.net>
2022-09-01 18:42:30 +0300

    Add dummy Rector config

---
+++ rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . "/src",
+        __DIR__ . "/tests",
+        __DIR__ . "/demo",
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_72,
+        // SetList::CODE_QUALITY,
+        // SetList::DEAD_CODE,
+        // SetList::PRIVATIZATION,
+        // SetList::NAMING,
+        // SetList::TYPE_DECLARATION,
+        // SetList::EARLY_RETURN,
+        // SetList::TYPE_DECLARATION_STRICT,
+        // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        // PHPUnitSetList::PHPUNIT_90,
+        // SetList::CODING_STYLE,
+    ]);
+};
